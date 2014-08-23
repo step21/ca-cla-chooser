a28128b1d187c8f16803ea15a980dc16aa37ae2c
step21 <step21@devtal.de>
2014-08-24 00:32:46 +0200

    more popover and changes

--- js/tooltip.js
+++ js/tooltip.js
@@ -3,5 +3,11 @@ $(document).ready(function() {
  	$('#popover_head').popover();
  	$('#entity_select').popover();
  	$('#indv_select').popover();
- 	$('#entity_name').popover();
+ 	$('#beneficiary_name').popover();
+  $('#project_name').popover();
+  $('#project_website').popover();
+  $('#project_email').popover();
+  $('#process_url').popover();
+  $('#project_jurisdiction').popover();
+
  });
