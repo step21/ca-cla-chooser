3def58d8c79dc1ba04223a62b79463fbb5559019
Jon Phillips <jon@fabricatorz.com>
2014-10-03 16:36:01 +0800

    remove useless pagedown

--- js/tooltip.js
+++ js/tooltip.js
@@ -2,6 +2,30 @@
 $(document).ready(function() {
  	$('#popover_head').popover({ placement: 'auto' });
   // maybe change all  below to bottom placement?
+ 	// $('#entity_select').popover({ placement: 'auto' });
+ 	$('#indv_select').popover({ placement: 'auto' });
+	$('#beneficiary-name-label').popover({ placement: 'auto' });
+  $('#project-name-label').popover({ placement: 'auto' });
+  $('#project-website-label').popover({ placement: 'auto' });
+  $('#project-email-label').popover({ placement: 'auto' });
+  $('#contributor-process-url-label').popover({ placement: 'auto' });
+  $('#project-jurisdiction-label').popover({ placement: 'auto' });
+
+  $('#agreement-exclusivity-label').popover({ placement: 'auto' });
+  $('#outbound-options-label').popover({ placement: 'auto' });
+  $('#outbound-option-1-label').popover({ placement: 'auto' });
+  $('#outbound-option_2-label').popover({ placement: 'auto' });
+  $('#outbound-option_3-label').popover({ placement: 'auto' });
+  $('#outbound-option_4-label').popover({ placement: 'auto' });
+  $('#patent-type-label').popover({ placement: 'auto' });
+  $('#outbound-media-label').popover({ placement: 'auto' });
+ });
+/*
+ * These work with bootstrap3 but not with current bootstrap 2.3.2
+ *
+$(document).ready(function() {
+ 	$('#popover_head').popover({ placement: 'auto' });
+  // maybe change all  below to bottom placement?
  	$('#entity_select').popover({ placement: 'auto' });
  	$('#indv_select').popover({ placement: 'auto' });
 	$('#beneficiary-name-label').popover({ placement: 'auto' });
@@ -20,3 +44,4 @@ $(document).ready(function() {
   $('#patent-type-label').popover({ placement: 'auto' });
   $('#outbound-media-label').popover({ placement: 'auto' });
  });
+*/
