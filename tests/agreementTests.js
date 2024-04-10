7da36ad29dcb94a43b984d514f872e9d64ca504b
step21 <step21@devtal.de>
2024-04-10 18:46:15 +0200

    some fixes, getting url parameters in test

--- tests/agreementTests.js
+++ tests/agreementTests.js
@@ -16,8 +16,11 @@ describe('The length of each document version should be correct', function() {
 
     it('the fla version (html) with default values should have the right length', async function() {
         await expect(agreementPage.applyResultHtmlFlaText).to.exist;
+        let t = await agreementPage.applyResultHtmlFlaText.getValue();
         let l = await agreementPage.getHtmlFlaTextLength();
+        console.log(`the type of t is: ${typeof t}`)
         console.log(`The FLA length is: ${l}`)
+        console.log(`The beginning of the text is ${t.slice(0, 24)}`)
         await expect(l).to.equal(17019);
 
     });
@@ -47,7 +50,7 @@ describe('The length of each document version should be correct', function() {
 
     });
     it('the query url without special options should have the same value', async function() {
-        var url = await agreementPage.applyResultLinkFla;
+        var url = await agreementPage.applyResultLinkFlaText;
         console.log(`url is: ${url}`);
         await expect(url).to.exist;
         var url_length = await agreementPage.getLinkFlaLength();
@@ -56,7 +59,19 @@ describe('The length of each document version should be correct', function() {
     })
 });
 
-
-
-
-
+// FIXME add proper testing of all parameters. add license-policy
+describe('the url parameters should be correct', async function () {
+    describe('all parameters should be present', async function () {
+         beforeEach(async function() {
+         await agreementPage.open();
+         await agreementPage.goThroughAll();
+     });
+        it('the amount of parameters should be the same', async function () {
+            var url = await agreementPage.applyResultLinkFlaText;
+            var searchParams = new URLSearchParams(url);
+            for (const p of searchParams) {
+                console.log(p);
+            }
+        })
+    })
+})
